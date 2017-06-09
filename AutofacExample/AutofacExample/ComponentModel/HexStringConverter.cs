0f46ba2d63a2ca128cef02247551f2bf1270b592
usausa <machi.pon@gmail.com>
2017-06-09 16:47:28 +0900

    Update HexStringConverter

--- AutofacExample/AutofacExample/ComponentModel/HexStringConverter.cs
+++ AutofacExample/AutofacExample/ComponentModel/HexStringConverter.cs
@@ -16,19 +16,19 @@
             var s = value as string;
             if (s != null)
             {
-                return Decode(s);
+                return Decode(s, culture);
             }
 
             return base.ConvertFrom(context, culture, value);
         }
 
-        private static byte[] Decode(string code)
+        private static byte[] Decode(string code, IFormatProvider culture)
         {
             var bytes = new byte[code.Length / 2];
 
             for (var index = 0; index < bytes.Length; index++)
             {
-                bytes[index] = byte.Parse(code.Substring(index * 2, 2), NumberStyles.HexNumber, CultureInfo.InvariantCulture);
+                bytes[index] = byte.Parse(code.Substring(index * 2, 2), NumberStyles.HexNumber, culture);
             }
 
             return bytes;
